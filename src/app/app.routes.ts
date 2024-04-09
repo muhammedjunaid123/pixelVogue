4f872efa1e9de35714af02afa18b46b2416c5141
junaid <junaidvinu133@gmail.com>
2024-04-09 21:19:10 +0530

    add the user side login signup otp home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/user/login/login.component';
+import { SignupComponent } from './components/user/signup/signup.component';
+import { OtpComponent } from './components/user/otp/otp.component';
+import { HomeComponent } from './components/user/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'otp', component: OtpComponent },
+    { path: 'home', component: HomeComponent }
+];
